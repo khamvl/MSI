2b8f29d0b0901282a139b31173935e440fba69fb
khamvl <114435144+khamvl@users.noreply.github.com>
2022-09-26 22:23:30 +0300

    Initial commit

--- Range/src/ru/academits/kharitonov/range_main/RangeMain.java
+++ Range/src/ru/academits/kharitonov/range_main/RangeMain.java
@@ -1,7 +1,42 @@
 package ru.academits.kharitonov.range_main;
 
+import ru.academits.kharitonov.range.Range;
+
 public class RangeMain {
     public static void main(String[] args) {
-        System.out.println("KK");
+        Range range = new Range(132.123, 14124.213);
+
+        double from = range.getFrom();
+        double to = range.getTo();
+        boolean isInRangeNumber = range.isInside(565.45);
+
+        System.out.println("Наименьшее число в диапазоне: " + from);
+        System.out.println("Наибольшее число в диапазоне: " + to);
+        System.out.println("Длина диапазана = " + range.getLength());
+
+        if (isInRangeNumber) {
+            System.out.println("Число входит в диапазон");
+        } else {
+            System.out.println("Число не входит в диапазон");
+        }
+
+        System.out.println("---------------------------------");
+
+        range.setFrom(555.213);
+        range.setTo(999.6453);
+
+        from = range.getFrom();
+        to = range.getTo();
+        isInRangeNumber = range.isInside(9564.45);
+
+        System.out.println("Наименьшее число в диапазоне: " + from);
+        System.out.println("Наибольшее число в диапазоне: " + to);
+        System.out.println("Длина диапазона = " + range.getLength());
+
+        if (isInRangeNumber) {
+            System.out.println("Число входит в диапазон");
+        } else {
+            System.out.println("Число не входит в диапазон");
+        }
     }
 }
