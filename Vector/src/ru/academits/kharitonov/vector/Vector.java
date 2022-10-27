d867fbb26543f4a4bce34946f04264a21a551eb6
MSI <khmvl0@gmail.com>
2022-10-27 21:52:27 +0300

    Creating module Vector

--- Vector/src/ru/academits/kharitonov/vector/Vector.java
+++ Vector/src/ru/academits/kharitonov/vector/Vector.java
@@ -3,10 +3,9 @@ package ru.academits.kharitonov.vector;
 import java.util.Arrays;
 
 public class Vector {
-    double[] arrayCopy;
-    int n;                          
+    double[] array;
+    int n;
     Vector vector;
-
     double[] values;
 
     public Vector(int n) {
@@ -31,22 +30,203 @@ public class Vector {
             throw new NegativeArraySizeException("n must be >= 0");
         }
 
-        arrayCopy = new double[n];
+        array = new double[n];
 
         for (int i = 0; i < n; i++) {
             try {
-                this.arrayCopy[i] = values[i];
+                this.array[i] = values[i];
             } catch (ArrayIndexOutOfBoundsException e) {
                 if (values.length < n) {
-                    arrayCopy[i] = 0;
+                    array[i] = 0;
                 }
             }
         }
     }
 
-
-    public String toString() {
-        return "" + Arrays.toString(arrayCopy);
+    public int getSize() {
+        return array.length;
     }
 
+    @Override
+    public String toString() {
+        int i = 0;
+        StringBuilder arrayString = new StringBuilder();
+
+        while (i < array.length) {
+            if (i == 0) {
+                arrayString = new StringBuilder(String.valueOf(array[i]));
+            } else {
+                arrayString.append(", ").append(array[i]);
+            }
+
+            i++;
+        }
+
+        return "{" + arrayString + "}";
+    }
+
+    public double[] getVectorAddition(Vector nValues2) {
+        int maxLength = Math.max(array.length, nValues2.array.length);
+        double[] resultVector = new double[maxLength];
+
+        for (int i = 0; i < maxLength; i++) {
+            try {
+                resultVector[i] = array[i] + nValues2.array[i];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                if (array.length < nValues2.array.length) {
+                    resultVector[i] = nValues2.array[i];
+                } else {
+                    resultVector[i] = array[i];
+                }
+            }
+        }
+
+        array = resultVector;
+
+        return array;
+    }
+
+    public double[] getVectorSubtraction(Vector nValues2) {
+        int maxLength = Math.max(array.length, nValues2.array.length);
+        double[] resultVector = new double[maxLength];
+
+        for (int i = 0; i < maxLength; i++) {
+            try {
+                resultVector[i] = array[i] - nValues2.array[i];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                if (array.length < nValues2.array.length) {
+                    resultVector[i] = -nValues2.array[i];
+                } else {
+                    resultVector[i] = -array[i];
+                }
+            }
+        }
+        array = resultVector;
+
+        return array;
+    }
+
+    public double[] getMultiplicationOfVectorByScalar(double coefficient) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] * coefficient;
+        }
+
+        return array;
+    }
+
+    public double[] getVectorReversal() {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] * -1;
+        }
+
+        return array;
+    }
+
+    public double getVectorComponent(int index) {
+        if (index < 0) {
+            throw new ArrayIndexOutOfBoundsException("index must be => 0");
+        }
+
+        if (index > array.length - 1) {
+            throw new ArrayIndexOutOfBoundsException("index must be < arrayLength");
+        } else {
+            return array[index];
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int hash = 1;
+
+        hash = prime * hash + Arrays.hashCode(array);
+        hash = prime * hash + n;
+        hash = prime * hash + (vector != null ? vector.hashCode() : 0);
+        hash = prime * hash + Arrays.hashCode(values);
+
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (o == null || o.getClass() != getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) o;
+
+        int maxLength = Math.max(array.length, vector.array.length);
+        boolean areEqual = true;
+
+        for (int i = 0; i < maxLength; i++) {
+            try {
+                if (array[i] != vector.array[i]) {
+                    areEqual = false;
+                    break;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return false;
+            }
+
+        }
+
+        return areEqual && n == vector.n;
+    }
+
+    public static double[] getVectorAddition(Vector nValues1, Vector nValues2) {
+        int maxLength = Math.max(nValues1.array.length, nValues2.array.length);
+        double[] resultVector = new double[maxLength];
+
+        for (int i = 0; i < maxLength; i++) {
+            try {
+                resultVector[i] = nValues1.array[i] + nValues2.array[i];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                if (nValues1.array.length < nValues2.array.length) {
+                    resultVector[i] = nValues2.array[i];
+                } else {
+                    resultVector[i] = nValues1.array[i];
+                }
+            }
+        }
+
+        return resultVector;
+    }
+
+    public static double[] getVectorSubtraction(Vector nValues1, Vector nValues2) {
+        int maxLength = Math.max(nValues1.array.length, nValues2.array.length);
+        double[] resultVector = new double[maxLength];
+
+        for (int i = 0; i < maxLength; i++) {
+            try {
+                resultVector[i] = nValues1.array[i] - nValues2.array[i];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                if (nValues1.array.length < nValues2.array.length) {
+                    resultVector[i] = -nValues2.array[i];
+                } else {
+                    resultVector[i] = -nValues1.array[i];
+                }
+            }
+        }
+
+        return resultVector;
+    }
+
+    public static double getDotVectorsProduct(Vector nValues1, Vector nValues2) {
+        int maxLength = Math.max(nValues1.array.length, nValues2.array.length);
+        double resultVector = 0;
+
+        for (int i = 0; i < maxLength; i++) {
+            try {
+                resultVector += nValues1.array[i] * nValues2.array[i];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                resultVector += 0;
+            }
+        }
+
+        return resultVector;
+    }
 }
--- Vector/src/ru/academits/kharitonov/vector_main/VectorMain.java
+++ Vector/src/ru/academits/kharitonov/vector_main/VectorMain.java
@@ -2,16 +2,33 @@ package ru.academits.kharitonov.vector_main;
 
 import ru.academits.kharitonov.vector.Vector;
 
+import java.util.Arrays;
+
 public class VectorMain {
     public static void main(String[] args) {
-        Vector vectorN = new Vector(2);
+        double[] array1 = {1.2, 3.2, 1.1, 5.2, 1.7};
+        Vector nValues1 = new Vector(5, array1);
+        System.out.println("Вектор 1: " + nValues1);
 
-        Vector vectorCopy = new Vector(vectorN);
+        double[] array2 = {1.2, 3.2, 1.1, 10.2, 1.7};
+        Vector nValues2 = new Vector(4, array2);
+        System.out.println("Вектор 2: " + nValues2);
 
-        double[] array = {1.2, 3.2, 5.1, 6.2};
-        Vector valuesArray = new Vector(array);
+        System.out.println("-----------------------------------");
 
-        Vector nValues = new Vector(-2, array);
-        System.out.println(nValues);
+        System.out.println("Статические:");
+        System.out.println("Сложение векторов: " + Arrays.toString(Vector.getVectorAddition(nValues1, nValues2)));
+        System.out.println("Вычитание векторов: " + Arrays.toString(Vector.getVectorSubtraction(nValues1, nValues2)));
+        System.out.println("Скалярное произведение векторов: " + Vector.getDotVectorsProduct(nValues1, nValues2));
+
+        System.out.println("------------------------------------");
+
+        System.out.println("Нестатические:");
+        System.out.println("Сумма векторов: " + Arrays.toString(nValues1.getVectorAddition(nValues2)));
+        System.out.println("Разность векторов: " + Arrays.toString(nValues1.getVectorSubtraction(nValues2)));
+        System.out.println("Умножение векторов: " + Arrays.toString(nValues1.getMultiplicationOfVectorByScalar(3)));
+        System.out.println("Разворот вектора: " + Arrays.toString(nValues1.getVectorReversal()));
+        System.out.println("Длина вектора: " + nValues2.getSize());
+        System.out.println("Компонента вектора по индексу: " + nValues1.getVectorComponent(2));
     }
 }
