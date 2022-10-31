3052de0f7c2aea83edf2c2e8bff83e86411177b5
MSI <khmvl0@gmail.com>
2022-10-31 23:21:52 +0300

    Correction of an error in the calculation

--- Range/src/ru/academits/kharitonov/range/Range.java
+++ Range/src/ru/academits/kharitonov/range/Range.java
@@ -67,11 +67,11 @@ public class Range {
             return new Range[]{new Range(from, to)};
         }
 
-        if (from < range.from && to < range.to) {
+        if (from < range.from && to <= range.to) {
             return new Range[]{new Range(from, range.from)};
         }
 
-        if (from > range.from && to > range.to) {
+        if (from >= range.from && to > range.to) {
             return new Range[]{new Range(range.to, to)};
         }
 
--- Range/src/ru/academits/kharitonov/range_main/RangeMain.java
+++ Range/src/ru/academits/kharitonov/range_main/RangeMain.java
@@ -43,10 +43,10 @@ public class RangeMain {
 
         System.out.println("---------------------------------");
 
-        range.setFrom(1);
+        range.setFrom(3);
         range.setTo(7);
 
-        Range range1 = new Range(3, 5);
+        Range range1 = new Range(5, 7);
 
         Range intersectionRange = range.getIntersection(range1);
 
