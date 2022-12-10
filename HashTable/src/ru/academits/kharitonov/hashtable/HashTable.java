60dadec0f11b69be8845c0da160d6b550a60595e
MSI <khmvl0@gmail.com>
2022-12-10 15:28:18 +0300

    Creating HashTable class

--- HashTable/src/ru/academits/kharitonov/hashtable/HashTable.java
+++ HashTable/src/ru/academits/kharitonov/hashtable/HashTable.java
@@ -5,30 +5,39 @@ import java.util.*;
 public class HashTable<T> implements Collection<T> {
     private ArrayList<T>[] list;
     private int size;
-
-    private ArrayList<T> iter;
+    private int modCount;
 
     public HashTable() {
+        list = new ArrayList[10];
     }
 
-    public void tableArray(int size) {
-        this.size = size;
+    public HashTable(int size) {
+        if (size <= 0) {
+            throw new NegativeArraySizeException("Size = " + size + ". But size must be > 0");
+        }
+
         list = new ArrayList[size];
     }
 
-    private int getIndex(T t) {
-        return Math.abs(t.hashCode() % list.length);
+    private int getIndex(Object o) {
+        if (o == null) {
+            return 0;
+        }
+
+        return Math.abs(o.hashCode() % list.length);
     }
 
     @Override
-    public boolean add(T t) {
-        int index = getIndex(t);
+    public boolean add(T value) {
+        int index = getIndex(value);
 
         if (list[index] == null) {
             list[index] = new ArrayList<>();
         }
 
-        list[index].add(t);
+        list[index].add(value);
+        size++;
+        modCount++;
 
         return true;
     }
@@ -45,93 +54,204 @@ public class HashTable<T> implements Collection<T> {
 
     @Override
     public boolean isEmpty() {
-        for (int i = 0; i < size; i++) {
-            if (list[i] != null) {
-                return true;
+        for (ArrayList<T> currentList : list) {
+            if (currentList != null) {
+                return false;
             }
         }
 
-        return false;
+        return true;
     }
 
     @Override
     public boolean contains(Object o) {
-        int index = getIndex((T) o);
-
-        if (list[index] == null) {
+        if (isEmpty()) {
             return false;
         }
 
-        return list[index].contains(o);
+        int index = getIndex(o);
+
+        return list[index] != null && list[index].contains(o);
     }
 
     @Override
     public Iterator<T> iterator() {
-        return new MyListIterator();
+        return new HashTableIterator();
     }
 
-    private class MyListIterator implements Iterator<T> {
-        private int currentIndex = -1;
+    private class HashTableIterator implements Iterator<T> {
+        private int start = modCount;
+        private int arrayIndex;
+        private int listIndex = -1;
+        private int totalElementalQuantity;
 
         public boolean hasNext() {
-            return currentIndex + 1 < size;
+            return totalElementalQuantity < size;
         }
 
         public T next() {
-            ++currentIndex;
-            return (T) list[currentIndex];
+            if (!hasNext()) {
+                throw new NoSuchElementException("The collection is over");
+            }
+
+            if (start != modCount) {
+                throw new ConcurrentModificationException("The number of items in the collection has changed during the crawl");
+            }
+
+            while (true) {
+                if (list[arrayIndex] == null) {
+                    arrayIndex++;
+                } else {
+                    listIndex++;
+
+                    if (listIndex >= list[arrayIndex].size()) {
+                        arrayIndex++;
+                        listIndex = -1;
+                    } else {
+                        totalElementalQuantity++;
+                        break;
+                    }
+                }
+            }
+
+            return list[arrayIndex].get(listIndex);
         }
     }
 
     @Override
     public Object[] toArray() {
-        Object[] newArray = new Object[20];
+        Object[] array = new Object[size];
+        int i = 0;
 
-        for (int i = 0; i < size; i++) {
-            if (list[i] == null) {
-                continue;
-            }
-
-            for (int j = 0; j < list[i].size(); j++) {
-                newArray[i] = list[i].get(j);
-            }
+        for (T value : this) {
+            array[i] = value;
+            i++;
         }
 
-        return newArray;
+        return array;
     }
 
     @Override
     public <T1> T1[] toArray(T1[] a) {
-        return null;
+        if (a.length < size) {
+            return (T1[]) Arrays.copyOf(toArray(), size);
+        }
+
+        T1[] temp = (T1[]) toArray();
+
+        for (int i = 0; i < size; i++) {
+            if (i < temp.length) {
+                a[i] = temp[i];
+            } else {
+                a[i] = null;
+            }
+        }
+
+        return a;
     }
 
     @Override
     public boolean remove(Object o) {
+        int index = getIndex(o);
+
+        if (contains(o)) {
+            list[index].remove(o);
+            size--;
+            modCount++;
+
+            return true;
+        }
+
         return false;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        for (Object value : c) {
+            if (!contains(value)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        if (c.isEmpty()) {
+            throw new NullPointerException("Collection is empty");
+        }
+
+        for (T value : c) {
+            add(value);
+        }
+
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        if (c.isEmpty()) {
+            throw new NullPointerException("Collection is empty");
+        }
+
+        for (Object value : c) {
+            remove(value);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (o == null || o.getClass() != getClass()) {
+            return false;
+        }
+
+        HashTable<T> hashTable = (HashTable<T>) o;
+
+        return list == hashTable.list && size == hashTable.size && modCount == hashTable.modCount;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        if (c.isEmpty()) {
+            return true;
+        }
+
+        boolean hasChange = false;
+
+        for (ArrayList<T> currentList : list) {
+            if (currentList != null) {
+                int startCurrentListSize = currentList.size();
+
+                if (currentList.retainAll(c)) {
+                    hasChange = true;
+                    size = size - startCurrentListSize - currentList.size();
+                }
+            }
+        }
+
+        return hasChange;
     }
 
     @Override
     public void clear() {
+        if (isEmpty()) {
+            return;
+        }
 
+        for (ArrayList<T> currentList : list) {
+            if (currentList != null) {
+                currentList.clear();
+            }
+        }
+
+        size = 0;
+        modCount++;
     }
 }
--- HashTable/src/ru/academits/kharitonov/hashtable_main/HashTableMain.java
+++ HashTable/src/ru/academits/kharitonov/hashtable_main/HashTableMain.java
@@ -1,46 +1,68 @@
 package ru.academits.kharitonov.hashtable_main;
 
-import ru.academits.kharitonov.hashtable.ListItem;
 import ru.academits.kharitonov.hashtable.HashTable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class HashTableMain {
     public static void main(String[] args) {
-        HashTable<ListItem<Integer>> myHashTable = new HashTable<>();
-        myHashTable.tableArray(10);
+        HashTable<String> myHashTable = new HashTable<>(5);
 
-        ListItem<Integer> value1 = new ListItem<>(111);
-        ListItem<Integer> value2 = new ListItem<>(222);
-        ListItem<Integer> value3 = new ListItem<>(333);
-        ListItem<Integer> value4 = new ListItem<>(444);
-        ListItem<Integer> value5 = new ListItem<>(555);
-        ListItem<Integer> value6 = new ListItem<>(666);
-        ListItem<Integer> value7 = new ListItem<>(111);
-
-        myHashTable.add(value1);
-        myHashTable.add(value2);
-        myHashTable.add(value3);
-        myHashTable.add(value4);
-        myHashTable.add(value5);
-        myHashTable.add(value6);
-        myHashTable.add(value7);
+        myHashTable.add(null);
+        myHashTable.add("1");
+        myHashTable.add("2");
+        myHashTable.add("3");
+        myHashTable.add("4");
+        myHashTable.add("5");
 
         System.out.println(myHashTable);
-        System.out.println("Длина хэш-таблицы = " + myHashTable.size());
+        System.out.println("---------------------------------");
 
         if (myHashTable.isEmpty()) {
-            System.out.println("Хэш таблица не пуста");
+            System.out.println("Таблица пуста");
         } else {
-            System.out.println("Хэш таблица пуста");
+            System.out.println("Таблица не пуста");
         }
 
-        if (myHashTable.contains(value5)) {
-            System.out.println("Хэш таблица содержит объект");
+        System.out.println("---------------------------------");
+
+        if (myHashTable.contains("0")) {
+            System.out.println("Таблица содержит объект");
         } else {
-            System.out.println("Хэш таблица не содержит объект");
+            System.out.println("Таблица не содержит объект");
         }
+
+        System.out.println("---------------------------------");
 
         System.out.println(Arrays.toString(myHashTable.toArray()));
+        System.out.println("---------------------------------");
+
+        if (myHashTable.remove("2")) {
+            System.out.println("Элемент удален");
+        } else {
+            System.out.println("Элемент не удален");
+        }
+
+        System.out.println(myHashTable);
+        System.out.println("---------------------------------");
+
+        myHashTable.addAll(new ArrayList<>(Arrays.asList("22", "33", "44")));
+        System.out.println(myHashTable);
+        System.out.println("---------------------------------");
+
+        myHashTable.removeAll(new ArrayList<>(Arrays.asList("4", "1")));
+        System.out.println(myHashTable);
+        System.out.println("---------------------------------");
+
+        System.out.println(Arrays.toString(myHashTable.toArray(new String[8])));
+        System.out.println("---------------------------------");
+
+        myHashTable.retainAll(new ArrayList<>((Arrays.asList("22", "5", "44"))));
+        System.out.println(myHashTable);
+        System.out.println("---------------------------------");
+
+        myHashTable.clear();
+        System.out.println(myHashTable);
     }
 }
