a159866025eb26306c7f93abaf3d2c3bc36d0e24
MSI <khmvl0@gmail.com>
2023-02-15 15:02:22 +0300

    Creating ArrayList class

--- Temperature/src/temperature_model/Model.java
+++
@@ -1,7 +0,0 @@
-package temperature_model;
-
-public interface Model {
-    Scale[] scales();
-
-    double convertTemperature(Scale inputScale, Scale outputScale, double inputTemperature);
-}
--- Temperature/src/temperature_model/Scale.java
+++
@@ -1,7 +0,0 @@
-package temperature_model;
-
-public interface Scale {
-    double convertFromCelsius(double temperature);
-
-    double convertInCelsius(double temperature);
-}
